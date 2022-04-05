02d870dabbd7e84e35f63c5b22225f59af5428e9
imsikun <imsikun16@gmail.com>
2022-04-05 15:26:45 +0530

    added the card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
 import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
+import Widgets from '../../components/widgets/Widgets';
 function Home() {
   return (
     <div className='home'>
       <Sidebar />
       <div className='homeContainer'>
         <Navbar />
-        <div className='widgets'></div>
+        <div className='widgets'>
+          <Widgets type='user' />
+          <Widgets type={'order'} />
+          <Widgets type={'earnings'} />
+          <Widgets type={'balance'} />
+        </div>
       </div>
     </div>
   );
